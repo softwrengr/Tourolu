b63f23388db687adafbb094ff59c2de0184e6634
eapple <softwrengr@gmail.com>
2020-06-10 00:48:15 +0400

    webview completed

--- app/src/main/java/com/squaredeveloper/tourolu/MainActivity.kt
+++ app/src/main/java/com/squaredeveloper/tourolu/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.squaredeveloper.tourolu
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.FrameLayout
+import android.widget.ProgressBar
 import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -36,12 +44,68 @@ class MainActivity : AppCompatActivity() {
                 loadWebview(Constant.visa)
                 return@OnNavigationItemSelectedListener true
             }
+            R.id.navigation_tours -> {
+                loadWebview(Constant.visa)
+                return@OnNavigationItemSelectedListener true
+            }
         }
         false
     }
 
     private fun loadWebview(url:String) {
+        val settings = webview.settings
         webview.loadUrl(url)
+        settings.javaScriptEnabled = true
+
+        // Enable and setup web view cache
+        settings.setAppCacheEnabled(true)
+        settings.cacheMode = WebSettings.LOAD_DEFAULT
+        settings.setAppCachePath(cacheDir.path)
+
+        settings.loadsImagesAutomatically = true
+
+        settings.useWideViewPort = true
+        settings.loadWithOverviewMode = true
+        settings.javaScriptCanOpenWindowsAutomatically = true
+        settings.mediaPlaybackRequiresUserGesture = false
+        settings.domStorageEnabled = true
+        settings.setSupportMultipleWindows(true)
+        settings.loadWithOverviewMode = true
+        settings.allowContentAccess = true
+        settings.setGeolocationEnabled(true)
+        settings.allowUniversalAccessFromFileURLs = true
+        settings.allowFileAccess = true
+        webview.fitsSystemWindows = true
+
+
+        webview.webViewClient = object: WebViewClient(){
+            override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
+                // Page loading started
+               progressbar.visibility = View.VISIBLE
+
+            }
+
+            override fun onPageFinished(view: WebView, url: String) {
+                // Page loading finished
+                progressbar.visibility = View.GONE
+            }
+        }
+
+        webview.webChromeClient = object: WebChromeClient(){
+            override fun onProgressChanged(view: WebView, newProgress: Int) {
+               // progress_bar.progress = newProgress
+
+            }
+        }
     }
 
+    // Handle back button press in web view
+    override fun onBackPressed() {
+        if (webview.canGoBack()) {
+            // If web view have back history, then go to the web view back history
+            webview.goBack()
+        } else {
+            finish()
+        }
+    }
 }
